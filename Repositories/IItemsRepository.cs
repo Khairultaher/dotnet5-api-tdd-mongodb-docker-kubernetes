065b439e39a75fbcf9cbc738fd2b790330a26a31
Khairul Taher <khairultaher@outlook.com>
2021-04-03 22:30:31 +0600

    first commit

---
+++ Repositories/IItemsRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
+    }
+}
